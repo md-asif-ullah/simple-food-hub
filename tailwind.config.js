b16484979728c4bd5ac93e18b653c89e605fe2be
MD.Asif Ullah <mdasifullah456@gmail.com>
2023-09-12 19:26:44 +0600

    development tools added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8aa899', 
+      }
+    },
+  },
+  plugins: [],
+}
